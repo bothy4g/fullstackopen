e115722914916dc92819aabd89a6cc7cb1fc292b
Kopacz Botond <bothy4g@yahoo.com>
2025-08-19 06:45:20 +0300

    Unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,36 +6,53 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const [average, setAverage] = useState(0)
 
-  const handleClick = (score_add) => {
-    switch(score_add){
-      case 1:
-        setGood(good + 1)
-      break;
-      case 0:
-        setNeutral(neutral + 1)
-      break;
-      case -1:
-        setBad(bad + 1)
-      break;
-    }
+  
+  const calcPositive = () => { 
+    console.log("All in setPOsitive", all)
+    good/all*100
   }
-
+  
+  const handleGoodClick = () => {
+    const newGood = good + 1;
+    const newAll = all + 1;
+    setGood(newGood)
+    setAll(newAll)
+    setPositive(newGood/newAll*100)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    const newAll = all + 1;
+    setAll(newAll)
+    setPositive(good/newAll*100)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    const newAll = all + 1;
+    setAll(newAll)
+    setPositive(good/newAll*100)
+  }
 
 
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <button onClick={handleGoodClick}>Good</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleBadClick}>Bad</button>
 
       <div>
         Statistics:
         <div>Good: {good}</div>
         <div>Neutral: {neutral}</div>
         <div>Bad: {bad}</div>
+        <div>All: {all}</div>
+        <div>Positive: {positive}%</div>
+        <div>All: {all}</div>
       </div>
     </div>
   )
