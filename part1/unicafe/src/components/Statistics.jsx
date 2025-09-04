ae14ffa6e1bfffff1a7ffb27af3360d9fe4f0b01
Kopacz Botond <bothy4g@yahoo.com>
2025-09-04 22:21:57 +0300

    Unicafe step 6

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -3,15 +3,17 @@ import StatisticLine from "./StatisticLine"
 const Statistics = ({good, bad, neutral, all, positive, average}) =>{
     if (all > 0)
         return(
-            <div>
-                Statistics:
-                <StatisticLine text="Good" value={good}/>
-                <StatisticLine text="Neutral" value={neutral}/>
-                <StatisticLine text="Bad" value={bad}/>
-                <StatisticLine text="All" value={all}/>
-                <StatisticLine text="Positive" value={positive + "%"}/>
-                <StatisticLine text="Average" value={average}/>
-            </div>
+            <table>
+                <thead><tr><td colSpan="2">Statistics</td></tr></thead>
+                <tbody>
+                    <StatisticLine text="Good" value={good}/>
+                    <StatisticLine text="Neutral" value={neutral}/>
+                    <StatisticLine text="Bad" value={bad}/>
+                    <StatisticLine text="All" value={all}/>
+                    <StatisticLine text="Positive" value={positive + "%"}/>
+                    <StatisticLine text="Average" value={average}/>
+                </tbody>
+            </table>
         )
     else 
         return <div>No feedback given.</div>
